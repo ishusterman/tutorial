fc4811ae2534add5278ff04384b8745f4fd6c90b
ishusterman2011 <ishusterman2011@gmail.com>
2024-12-27 13:15:40 +0200

    update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const DOCUMENTATION_OPTIONS = {
     VERSION: '1.1',
     LANGUAGE: 'en',
@@ -11,18 +10,4 @@ const DOCUMENTATION_OPTIONS = {
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
-=======
-const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.1',
-    LANGUAGE: 'en',
-    COLLAPSE_INDEX: false,
-    BUILDER: 'html',
-    FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
-    HAS_SOURCE: false,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
-    SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
->>>>>>> f39ef64 (Initial commit)
 };
