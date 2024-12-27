153ba2268c7361e7ef063743f720d01a2da82c3d
ishusterman2011 <ishusterman2011@gmail.com>
2024-12-27 13:09:32 +0200

    Initial commit

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const DOCUMENTATION_OPTIONS = {
     VERSION: '1.1',
     LANGUAGE: 'en',
@@ -10,4 +11,18 @@ const DOCUMENTATION_OPTIONS = {
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
+=======
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.1',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+>>>>>>> f39ef64 (Initial commit)
 };
